23c0d9cb343b59f60f9f441670faba962fc1e97a
Khyber Sen <kkysen@gmail.com>
2025-08-14 12:30:15 -0400

    setup basic CLI

--- omny-summary.py
+++ omny-summary.py
@@ -1,7 +1,18 @@
 #!/usr/bin/env -S uv run --script
+# /// script
+# requires-python = ">=3.13"
+# dependencies = [
+#     "pandas",
+#     "typer",
+# ]
+# ///
 
-def main():
-    pass
+from pathlib import Path
+import typer
+
+
+def main(trip_history_path: Path):
+    print(trip_history_path)
 
 if __name__ == "__main__":
-    main()
+    typer.run(main)
